5f8a9ae214c3c845dfe957451326c080cacfccb2
akabbeke <akabbeke@gmail.com>
2015-05-09 11:35:47 -0400

    Revert "Revert "first build""
    This reverts commit af884968b3d05246695f238584aa66a8b465af4d.

---
+++ Assets/Resources/Scripts/BulletController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject,5f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
